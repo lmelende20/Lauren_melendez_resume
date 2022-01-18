da0125dcb33aeb1ff7e6b0197a4de2ebe0022cd6
Lauren Melendez <lmelende@stevens.edu>
2022-01-17 21:23:19 -0500

    small margin fixes

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -5,16 +5,14 @@ function HeroSection() {
     return (
         <div className='flex flex-col justify-center h-full'>
             <div className="flex items-center justify-around md:flex-row flex-col-reverse">
-            <div className="mr-[20px] md:text-left text-center">
-                <h1 className="lg:text-[45px] md:text-[35px] text-[25px] font-Lora">Hello! I’m Lauren Melendez!</h1>
-                <h2 className="lg:text-[20px] text-[13px] font-Nunito">I am a UI/UX Designer, Frontend Developer, and Graphic Designer.</h2>
-            </div>
-            <div className="md:w-1/3 w-full max-w-[300px] flex justify-center md:block relative mb-[32px] md:mb-0">
-                <Image src='/Images/blob1.png' alt='blob' layout="intrinsic" objectFit="scale-down" width={481} height={453}/>
-                <div className="absolute top-0">
-                    <Image src='/Images/Mask_Group.svg' alt='selfPortrait' layout="intrinsic" objectFit="scale-down" width={481} height={453}/>
+                <div className="mr-[20px] md:text-left text-center">
+                    <h1 className="lg:text-[45px] md:text-[35px] text-[25px] font-Lora">Hello! I’m Lauren Melendez!</h1>
+                    <h2 className="lg:text-[20px] text-[13px] font-Nunito">I am a UI/UX Designer, Frontend Developer, and Graphic Designer.</h2>
                 </div>
-            </div>
+                
+                <div className="md:w-1/3 w-full max-w-[300px] flex justify-center md:block relative mb-[32px] md:mb-0">
+                    <Image src="/Images/SelfPortrait2.png" alt='Self Portrait Illustration' layout="intrinsic" objectFit="scale-down" width={481} height={453}/>
+                </div>
             </div>
         </div>
     )
--- components/Skills.js
+++ components/Skills.js
@@ -18,78 +18,78 @@ export default function () {
             <div className='w-screen bg-whitePink lg:py-[20px] py-[12px]'>
                 <h1 className='lg:text-[35px] md:text-[30px] text-[25px] px-[10px] font-Lora font-bold'>Skills</h1>
             </div>
-            <div className='flex flex-col justify-center items-center lg:mt-[40px] md:mt-[30px] mt-[20px]'>
+            <div className='flex flex-col justify-center items-center lg:mt-[40px] md:mt-[30px] mt-[20px] md:ml-[0] ml-[20px]'>
                 <ul>
                     <li>
                         <div className='flex my-[20px]'>
-                            <ViewQuiltIcon className='text-white font-Nunito lg:text-[30px] md:text-[25px] text-[20px] lg:mr-[10px] mr-[5px]'/>
-                            <h3 className='text-white font-Nunito lg:text-[20px] md:text-[15px] text-[10px]'>Frontend Development (Javascript, HTML, CSS)</h3>
+                            <ViewQuiltIcon className='text-[#AB3B61] font-Nunito lg:text-[30px] md:text-[25px] text-[20px] lg:mr-[10px] mr-[5px]'/>
+                            <h3 className='text-[#AB3B61] font-Nunito lg:text-[20px] md:text-[15px] text-[10px]'>Frontend Development (Javascript, HTML, CSS)</h3>
                         </div>
                     </li>
                     <li>
                         <div className='flex my-[20px]'>
-                            <DesignServicesIcon className='text-white font-Nunito lg:text-[30px] md:text-[25px] text-[20px] lg:mr-[10px] mr-[5px]'/>
-                            <h3 className='text-white font-Nunito lg:text-[20px] md:text-[15px] text-[10px]'>UI/UX Design (Adobe XD, Figma)</h3>
+                            <DesignServicesIcon className='text-[#AB3B61] font-Nunito lg:text-[30px] md:text-[25px] text-[20px] lg:mr-[10px] mr-[5px]'/>
+                            <h3 className='text-[#AB3B61] font-Nunito lg:text-[20px] md:text-[15px] text-[10px]'>UI/UX Design (Adobe XD, Figma)</h3>
                         </div>
                     </li>
                     <li>
                         <div className='flex my-[20px]'>
-                            <ComputerIcon className='text-white font-Nunito lg:text-[30px] md:text-[25px] text-[20px] lg:mr-[10px] mr-[5px]'/>
-                            <h3 className='text-white font-Nunito lg:text-[20px] md:text-[15px] text-[10px]'>Java, Python, C#, C, SQL, Erlang, OCaml, and Groovy</h3>
+                            <ComputerIcon className='text-[#AB3B61] font-Nunito lg:text-[30px] md:text-[25px] text-[20px] lg:mr-[10px] mr-[5px]'/>
+                            <h3 className='text-[#AB3B61] font-Nunito lg:text-[20px] md:text-[15px] text-[10px]'>Java, Python, C#, C, SQL, Erlang, OCaml, and Groovy</h3>
                         </div>
                     </li>
                     <li>
                         <div className='flex my-[20px]'>
-                            <BrushIcon className='text-white font-Nunito lg:text-[30px] md:text-[25px] text-[20px] lg:mr-[10px] mr-[5px]'/>
-                            <h3 className='text-white font-Nunito lg:text-[20px] md:text-[15px] text-[10px]'>Graphic Design (Adobe Illustrator, Adobe Photoshop, Adobe InDesign, Procreate)</h3>
+                            <BrushIcon className='text-[#AB3B61] font-Nunito lg:text-[30px] md:text-[25px] text-[20px] lg:mr-[10px] mr-[5px]'/>
+                            <h3 className='text-[#AB3B61] font-Nunito lg:text-[20px] md:text-[15px] text-[10px]'>Graphic Design (Adobe Illustrator, Adobe Photoshop, Adobe InDesign, Procreate)</h3>
                         </div>
                     </li>
                     <li>
                         <div className='flex my-[20px]'>
-                            <KeyboardIcon className='text-white font-Nunito lg:text-[30px] md:text-[25px] text-[20px] lg:mr-[10px] mr-[5px]'/>
-                            <h3 className='text-white font-Nunito lg:text-[20px] md:text-[15px] text-[10px]'>Functional Programming</h3>
+                            <KeyboardIcon className='text-[#AB3B61] font-Nunito lg:text-[30px] md:text-[25px] text-[20px] lg:mr-[10px] mr-[5px]'/>
+                            <h3 className='text-[#AB3B61] font-Nunito lg:text-[20px] md:text-[15px] text-[10px]'>Functional Programming</h3>
                         </div>
                     </li>
                     <li>
                         <div className='flex my-[20px]'>
-                            <AnimationIcon className='text-white font-Nunito lg:text-[30px] md:text-[25px] text-[20px] lg:mr-[10px] mr-[5px]'/>
-                            <h3 className='text-white font-Nunito lg:text-[20px] md:text-[15px] text-[10px]'>Motion Graphics (Adobe After Effects, Cinema 4D)</h3>
+                            <AnimationIcon className='text-[#AB3B61] font-Nunito lg:text-[30px] md:text-[25px] text-[20px] lg:mr-[10px] mr-[5px]'/>
+                            <h3 className='text-[#AB3B61] font-Nunito lg:text-[20px] md:text-[15px] text-[10px]'>Motion Graphics (Adobe After Effects, Cinema 4D)</h3>
                         </div>
                     </li>
                     <li>
                         <div className='flex my-[20px]'>
-                            <DeviceHubIcon className='text-white font-Nunito lg:text-[30px] md:text-[25px] text-[20px] lg:mr-[10px] mr-[5px]'/>
-                            <h3 className='text-white font-Nunito lg:text-[20px] md:text-[15px] text-[10px]'>Adept with all things technology</h3>
+                            <DeviceHubIcon className='text-[#AB3B61] font-Nunito lg:text-[30px] md:text-[25px] text-[20px] lg:mr-[10px] mr-[5px]'/>
+                            <h3 className='text-[#AB3B61] font-Nunito lg:text-[20px] md:text-[15px] text-[10px]'>Adept with all things technology</h3>
                         </div>
                     </li>
                     <li>
                         <div className='flex my-[20px]'>
-                            <SortIcon className='text-white font-Nunito lg:text-[30px] md:text-[25px] text-[20px] lg:mr-[10px] mr-[5px]'/>
-                            <h3 className='text-white font-Nunito lg:text-[20px] md:text-[15px] text-[10px]'>Organized and timely worker</h3>
+                            <SortIcon className='text-[#AB3B61] font-Nunito lg:text-[30px] md:text-[25px] text-[20px] lg:mr-[10px] mr-[5px]'/>
+                            <h3 className='text-[#AB3B61] font-Nunito lg:text-[20px] md:text-[15px] text-[10px]'>Organized and timely worker</h3>
                         </div>
                     </li>
                     <li>
                         <div className='flex my-[20px]'>
-                            <PeopleAltIcon className='text-white font-Nunito lg:text-[30px] md:text-[25px] text-[20px] lg:mr-[10px] mr-[5px]'/>
-                            <h3 className='text-white font-Nunito lg:text-[20px] md:text-[15px] text-[10px]'>Interpersonal</h3>
+                            <PeopleAltIcon className='text-[#AB3B61] font-Nunito lg:text-[30px] md:text-[25px] text-[20px] lg:mr-[10px] mr-[5px]'/>
+                            <h3 className='text-[#AB3B61] font-Nunito lg:text-[20px] md:text-[15px] text-[10px]'>Interpersonal</h3>
                         </div>
                     </li>
                     <li>
                         <div className='flex my-[20px]'>
-                            <TranslateIcon className='text-white font-Nunito lg:text-[30px] md:text-[25px] text-[20px] lg:mr-[10px] mr-[5px]'/>
-                            <h3 className='text-white font-Nunito lg:text-[20px] md:text-[15px] text-[10px]'>Experience reading, writing, and speaking Spanish</h3>
+                            <TranslateIcon className='text-[#AB3B61] font-Nunito lg:text-[30px] md:text-[25px] text-[20px] lg:mr-[10px] mr-[5px]'/>
+                            <h3 className='text-[#AB3B61] font-Nunito lg:text-[20px] md:text-[15px] text-[10px]'>Experience reading, writing, and speaking Spanish</h3>
                         </div>
                     </li>
                     <li>
                         <div className='flex my-[20px]'>
-                            <GroupWorkIcon className='text-white font-Nunito lg:text-[30px] md:text-[25px] text-[20px] lg:mr-[10px] mr-[5px]'/>
-                            <h3 className='text-white font-Nunito lg:text-[20px] md:text-[15px] text-[10px]'>Office 365</h3>
+                            <GroupWorkIcon className='text-[#AB3B61] font-Nunito lg:text-[30px] md:text-[25px] text-[20px] lg:mr-[10px] mr-[5px]'/>
+                            <h3 className='text-[#AB3B61] font-Nunito lg:text-[20px] md:text-[15px] text-[10px]'>Office 365</h3>
                         </div>
                     </li>
                     <li>
                         <div className='flex my-[20px]'>
-                            <GoogleIcon className='text-white font-Nunito lg:text-[30px] md:text-[25px] text-[20px] lg:mr-[10px] mr-[5px]'/>
-                            <h3 className='text-white font-Nunito lg:text-[20px] md:text-[15px] text-[10px]'>Google Workspace</h3>
+                            <GoogleIcon className='text-[#AB3B61] font-Nunito lg:text-[30px] md:text-[25px] text-[20px] lg:mr-[10px] mr-[5px]'/>
+                            <h3 className='text-[#AB3B61] font-Nunito lg:text-[20px] md:text-[15px] text-[10px]'>Google Workspace</h3>
                         </div>
                     </li>
                     
