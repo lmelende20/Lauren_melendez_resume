f6e06a0d6669b7060d2e698528450f58d257cd11
Lauren Melendez <lmelende@stevens.edu>
2022-02-03 16:40:01 -0500

    added head title

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,42 @@
 import '../styles/globals.css'
+import Head from "next/head"
+import { useRouter } from 'next/router'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+
+  function pageName(pathname){
+    if (pathname === '/'){
+      return 'Home'
+    }
+    else if (pathname === '/aboutme'){
+      return 'About'
+    }
+    else if (pathname === '/certifications'){
+      return 'Certifications'
+    }
+    else if (pathname === '/projects'){
+      return 'Projects'
+    }
+    else if (pathname === '/resume'){
+      return 'Resume'
+    }
+    else{
+      return ''
+    }
+
+  }
+
+  return (
+    <div>
+      <Head>
+        <title>{pageName(router.pathname)} | Lauren Melendez Profile</title>
+      </Head>
+      <Component {...pageProps} />
+    </div>
+    
+  )
+  
 }
 
 export default MyApp
