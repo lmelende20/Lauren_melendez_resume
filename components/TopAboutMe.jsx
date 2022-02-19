6251c95548ab32857894f1f439ee336ef1ddb064
Lauren Melendez <lmelende@stevens.edu>
2022-02-18 21:33:54 -0500

    fix

--- components/TopAboutMe.jsx
+++ components/TopAboutMe.jsx
@@ -5,7 +5,7 @@ function TopAboutMe() {
     return (
         <div className='flex flex-col justify-center items-center -my-8 '>         
             <div className="bg-[url('../public/Images/blob2.png')] bg-center bg-contain bg-no-repeat p-32">
-                <p className={`font-Nunito lg:text-[20px] md:text-[18px] sm:text-[15px] text-[12px] text-white top-0 lg:w-[400px] md:w-[350px] sm:w-[300px] w-[250px] text-center`}>
+                <p className={`font-Nunito lg:text-[18px] md:text-[16px] sm:text-[14px] text-[12px] text-white top-0 lg:w-[410px] md:w-[375px] sm:w-[300px] w-[250px] text-center`}>
                 My goal is to join a team at a software development firm where I can fully utilize both my software 
                 engineering and digital art skills. I believe that my experience and strengths in software development 
                 as well as design would make me an asset to any firm. Stevens Institute has been the perfect environment 
